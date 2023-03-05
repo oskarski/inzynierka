67442497f28de4a128a67670cdbef07de9b0cd4e
Oskar Kupski <oskar.kupski@gmail.com>
2023-03-05 17:16:58 +0100

    added basic backend app setup

---
+++ apps/backend/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import appConfig from './config/app.config';
+import dbConfig from './config/db.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: false,
+        host: configService.get('database').host,
+        port: configService.get('database').port,
+        username: configService.get('database').username,
+        password: configService.get('database').password,
+        database: configService.get('database').name,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
