7e158d7498fbab522954f931e60010a1b592276b
Oskar Kupski <oskar.kupski@gmail.com>
2023-03-09 22:33:20 +0100

    added roboto font

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,8 +1,13 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--adm-font-family)', ...fontFamily.sans],
+      },
       aspectRatio: {
         image: '396 / 226',
       },
