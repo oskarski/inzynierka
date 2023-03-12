5c3db6cac58e2259e106901112ce8d33d57f4086
Oskar Kupski <oskar.kupski@gmail.com>
2023-03-12 19:50:00 +0100

    added allowed origins for CORS

--- apps/backend/src/app/config/app.config.ts
+++ apps/backend/src/app/config/app.config.ts
@@ -1,3 +1,4 @@
 export default () => ({
   port: parseInt(process.env.APP_PORT, 10) || 3000,
+  allowedOrigins: process.env.ALLOWED_ORIGINS,
 });
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import appConfig from './app/config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = appConfig();
+
+  app.enableCors({ origin: config.allowedOrigins });
 
   await app.listen(process.env.APP_PORT);
 }
