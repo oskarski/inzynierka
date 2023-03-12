00ddd7914e6a8671e3b1109c74ffcb3b8a572b78
Oskar Kupski <oskar.kupski@gmail.com>
2023-03-12 15:04:32 +0100

    added base categories page

---
+++ apps/frontend/src/pages/categories.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import { headTitle } from '@fe/utils';
+
+export default function CategoriesPage() {
+  return (
+    <>
+      <Head>
+        <title>{headTitle('Kategorie')}</title>
+      </Head>
+
+      <main>CATEGORIES PAGE WILL BE HERE</main>
+    </>
+  );
+}
--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { headTitle } from '@fe/utils';
+import { SectionTitle } from '@fe/components';
 
 export default function HomePage() {
   return (
@@ -8,7 +9,9 @@ export default function HomePage() {
         <title>{headTitle()}</title>
       </Head>
 
-      <main>HOME PAGE WILL BE HERE</main>
+      <main>
+        <SectionTitle href="/categories">Popularne kategorie</SectionTitle>
+      </main>
     </>
   );
 }
