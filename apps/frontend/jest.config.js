3ab9684c3a012cbaa3ebd1b1aa990acfa6d4fddd
Oskar Kupski <oskar.kupski@gmail.com>
2023-03-24 22:17:47 +0100

    added testing libs

---
+++ apps/frontend/jest.config.js
@@ -0,0 +1,18 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleNameMapper: {
+    '@fe/(.*)': '<rootDir>/src/$1',
+  },
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
