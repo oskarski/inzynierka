72ace94cb2cbf6d425223657d38bc10753ff2759
Oskar Kupski <oskar.kupski@gmail.com>
2023-04-01 12:59:19 +0200

    added `<AppForm />`

---
+++ apps/frontend/src/form/AppForm.tsx
@@ -0,0 +1,41 @@
+import { Form } from 'antd-mobile';
+import {
+  ApiErrorMessage,
+  FormValidationOrApiError,
+  FormValidationSummaryErrorMessage,
+} from '@fe/errors';
+import { PropsWithChildren, ReactNode } from 'react';
+
+interface AppFormProps {
+  onSubmit: (values: unknown) => void;
+  submitBtn: ReactNode;
+  error: FormValidationOrApiError | null;
+}
+
+export const AppForm = ({
+  error,
+  onSubmit,
+  submitBtn,
+  children,
+}: PropsWithChildren<AppFormProps>) => {
+  return (
+    <Form
+      onFinish={onSubmit}
+      footer={
+        <>
+          {submitBtn}
+
+          <FormValidationSummaryErrorMessage error={error} className="mt-2" />
+          <ApiErrorMessage error={error} className="mt-2" />
+        </>
+      }
+      style={{
+        '--border-inner': '0',
+        '--border-top': '0',
+        '--border-bottom': '0',
+      }}
+    >
+      {children}
+    </Form>
+  );
+};
