d0d056f602108d6ce5d7470774d4f9bd9c3af1ab
Oskar Kupski <oskar.kupski@gmail.com>
2023-04-02 14:48:01 +0200

    fixed typo in SQL script

--- crawler/ingredientsCrawler.py
+++ crawler/ingredientsCrawler.py
@@ -59,7 +59,7 @@ for link in links:
     recipe_id +=1
 
 # Create a new table with unique ingredient names
-cursor.execute("CREATE TABLE unique_ingredients AS SELECT DISTINCT regexp_replace(trim(name), '\s{2,}', ' ', 'g') AS name FROM ingredients WHERE name !~ '[0-9]' AND name !~ '[1-9][/][0-9]' AND name !~ '[^\s]*\u00BD[^\s]*';)
+cursor.execute("CREATE TABLE unique_ingredients AS SELECT DISTINCT regexp_replace(trim(name), '\s{2,}', ' ', 'g') AS name FROM ingredients WHERE name !~ '[0-9]' AND name !~ '[1-9][/][0-9]' AND name !~ '[^\s]*\u00BD[^\s]*';")
 
 # Commit the changes to the database
 conn.commit()
