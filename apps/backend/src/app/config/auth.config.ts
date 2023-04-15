83b0e5fbc139509e0e7dfb9cbf529c9771e77ee3
Oskar Kupski <oskar.kupski@gmail.com>
2023-04-15 07:40:09 +0200

    added `clientId` env

--- apps/backend/src/app/config/auth.config.ts
+++ apps/backend/src/app/config/auth.config.ts
@@ -3,5 +3,6 @@ export default () => ({
     region: process.env.COGNITO_REGION,
     userPoolId: process.env.COGNITO_USER_POOL_ID,
     authority: `https://cognito-idp.${process.env.COGNITO_REGION}.amazonaws.com/${process.env.COGNITO_USER_POOL_ID}`,
+    clientId: process.env.COGNITO_CLIENT_ID,
   },
 });
