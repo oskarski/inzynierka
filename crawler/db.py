8540290a75732a09946475bc2fc2586868d666bf
JakubOstaszewski <101040302+JakubOstaszewski@users.noreply.github.com>
2023-04-23 12:25:55 +0200

    added script for table recipes

---
+++ crawler/db.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+# establish a connection to the database
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    user="root",
+    password="root",
+    database="inzynierka"
+)
+
+# create a cursor object to execute SQL statements
+cur = conn.cursor()
+
+# execute SQL statements to create the new table and populate it
+cur.execute("DROP TABLE IF EXISTS recipes")
+
+cur.execute("CREATE TABLE recipes (id SERIAL PRIMARY KEY, name VARCHAR(255), description TEXT, preparation_time INTEGER, portions INTEGER)")
+
+cur.execute("INSERT INTO recipes (name, description, preparation_time, portions) SELECT title, description, (substring(time from 1 for 1)::INTEGER * 3600) + (substring(time from 3)::INTEGER * 60), CAST(size AS INTEGER) FROM crawler_recipes WHERE trim(time) <> '' AND trim(size) <> ''")
+
+# commit the changes to the database and close the cursor and connection
+conn.commit()
+cur.close()
+conn.close()
