0feb38bea349f1ab852b6f7a70030cb75cc88cce
Oskar Kupski <oskar.kupski@gmail.com>
2023-04-25 15:37:26 +0200

    added favourite recipes page

---
+++ apps/frontend/__tests__/pages/FavouriteRecipesPage.test.tsx
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+import { TestContext } from '../utils';
+import FavouriteRecipesPage from '@fe/pages/favourite';
+
+const testContext = new TestContext();
+
+describe(FavouriteRecipesPage.name, () => {
+  it.todo('renders favourite recipes');
+});
--- apps/frontend/src/pages/favourite.tsx
+++ apps/frontend/src/pages/favourite.tsx
@@ -1,21 +1,61 @@
 import Head from 'next/head';
-import { headTitle } from '@fe/utils';
+import { env, headTitle, HttpClient } from '@fe/utils';
 import { GetServerSideProps } from 'next';
 import { SignedInGuard } from '../server/server-guards';
 import { HydrateReactQueryState } from '../server/server-react-query';
+import { Loader, SectionTitle } from '@fe/components';
+import {
+  ListFavouriteRecipesQueryKey,
+  RecipeCard,
+  RecipesApi,
+  useConnectedCategories,
+  useListFavouriteRecipes,
+} from '@fe/recipes';
+import { ApiErrorMessage } from '@fe/errors';
+import React from 'react';
 
 export const getServerSideProps: GetServerSideProps = HydrateReactQueryState(
-  SignedInGuard()
+  SignedInGuard(async (ctx, queryClient, user) => {
+    const recipesApi = new RecipesApi(
+      HttpClient.privateHttpClient(env().apiUrl, {
+        accessToken: user.accessToken,
+      })
+    );
+
+    await queryClient.prefetchQuery(ListFavouriteRecipesQueryKey, () =>
+      recipesApi.listFavouriteRecipes()
+    );
+
+    return { props: {} };
+  })
 );
 
-export default function FavouritePage() {
+export default function FavouriteRecipesPage() {
+  const [favouriteRecipes, loading, error] = useListFavouriteRecipes();
+
+  const connectedCategories = useConnectedCategories();
+
   return (
     <>
       <Head>
-        <title>{headTitle('Lista zakupów')}</title>
+        <title>{headTitle('Ulubione Przepisy')}</title>
       </Head>
 
-      <main>FAVOURITE PAGE WILL BE HERE</main>
+      <main>
+        <SectionTitle className="mb-6">Ulubione przepisy</SectionTitle>
+
+        {loading && <Loader />}
+        {error && <ApiErrorMessage size="base" error={error} />}
+
+        {favouriteRecipes?.map((recipe) => (
+          <RecipeCard
+            key={recipe.id}
+            recipe={recipe}
+            categories={connectedCategories(recipe)}
+            className="mb-4"
+          />
+        ))}
+      </main>
     </>
   );
 }
