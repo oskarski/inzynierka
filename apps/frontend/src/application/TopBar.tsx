461a57bdb30345f791715429636b82f40fff0c54
Oskar Kupski <oskar.kupski@gmail.com>
2023-04-29 13:48:11 +0200

    dropped dummy user avatar

--- apps/frontend/src/application/TopBar.tsx
+++ apps/frontend/src/application/TopBar.tsx
@@ -37,7 +37,7 @@ function ProfileAvatar() {
       <AppPopup.TriggerButton className="inline-block">
         {/*TODO Replace with current user avatar*/}
         <Avatar
-          src="https://images.unsplash.com/photo-1548532928-b34e3be62fc6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
+          src=""
           className="ml-auto"
           style={{ '--size': '48px', '--border-radius': '100%' }}
         />
