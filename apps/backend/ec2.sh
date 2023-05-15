da9622154b41c47f751816a87d2c417ab1ae1d1d
Oskar Kupski <oskar.kupski@gmail.com>
2023-05-15 22:44:25 +0200

    adjusted

--- apps/backend/ec2.sh
+++ apps/backend/ec2.sh
@@ -5,7 +5,7 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-sudo . ~/.nvm/nvm.sh
+. ~/.nvm/nvm.sh
 nvm install 16.0
 
 cd ~/inzynierka/lib/shared
