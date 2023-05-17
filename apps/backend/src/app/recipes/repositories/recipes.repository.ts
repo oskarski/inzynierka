63c51004874758cba9acad56a3140d8353b2cd94
Oskar Kupski <oskar.kupski@gmail.com>
2023-05-17 19:46:51 +0200

    listing empty array when no results

--- apps/backend/src/app/recipes/repositories/recipes.repository.ts
+++ apps/backend/src/app/recipes/repositories/recipes.repository.ts
@@ -260,7 +260,7 @@ class ListRecipesQuery {
   }
 
   getRawMany(): Promise<ListRecipesQueryResult[]> {
-    return this.queryBuilder.getRawMany() || [];
+    return this.queryBuilder.getRawMany() || Promise.resolve([]);
   }
 }
 
