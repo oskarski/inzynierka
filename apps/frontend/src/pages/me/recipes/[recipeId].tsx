3db64e16bd817d23ffdcbb2f838c3b146d4f03d4
Oskar Kupski <oskar.kupski@gmail.com>
2023-05-17 20:25:23 +0200

    added prefetch to recipe edit page

--- apps/frontend/src/pages/me/recipes/[recipeId].tsx
+++ apps/frontend/src/pages/me/recipes/[recipeId].tsx
@@ -1,17 +1,47 @@
 import Head from 'next/head';
-import { headTitle } from '@fe/utils';
+import { env, headTitle, HttpClient } from '@fe/utils';
 import { Loader, SectionTitle } from '@fe/components';
 import { HydrateReactQueryState } from '../../../server/server-react-query';
 import { SignedInGuard } from '../../../server/server-guards';
 import { GetServerSideProps } from 'next/types';
 import React from 'react';
-import { RecipeForm, useRecipeDetails } from '@fe/recipes';
+import {
+  GetRecipeDetailsQueryKey,
+  RecipeForm,
+  RecipesApi,
+  useRecipeDetails,
+} from '@fe/recipes';
 import { RecipeId } from '@lib/shared';
 import { ApiErrorMessage } from '@fe/errors';
+import {
+  ListAllRecipesCategoriesQueryKey,
+  RecipesCategoriesApi,
+} from '@fe/recipes-categories';
+import { IngredientsApi, ListIngredientsQueryKey } from '@fe/ingredients';
 
 export const getServerSideProps: GetServerSideProps = HydrateReactQueryState(
-  SignedInGuard(async ({ params }) => {
+  SignedInGuard(async ({ params }, queryClient, user) => {
     const recipeId: RecipeId = params?.recipeId as RecipeId;
+
+    const httpClient = HttpClient.privateHttpClient(env().apiUrl, {
+      accessToken: user.accessToken,
+    });
+    const recipesApi = new RecipesApi(httpClient);
+    const recipesCategoriesApi = new RecipesCategoriesApi(httpClient);
+    const ingredientsApi = new IngredientsApi(httpClient);
+
+    await queryClient.prefetchQuery(GetRecipeDetailsQueryKey(recipeId), () =>
+      recipesApi.getRecipeDetails(recipeId)
+    );
+    await queryClient.prefetchQuery(ListAllRecipesCategoriesQueryKey, () =>
+      recipesCategoriesApi.listCategories({})
+    );
+
+    const ingredientsQueryDto = { name: '' };
+    await queryClient.prefetchQuery(
+      ListIngredientsQueryKey(ingredientsQueryDto),
+      () => ingredientsApi.listIngredients(ingredientsQueryDto)
+    );
 
     return { props: { recipeId } };
   })
