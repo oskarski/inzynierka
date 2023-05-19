e609f66be992941266f329789a93d410f62fd48c
Oskar Kupski <oskar.kupski@gmail.com>
2023-05-19 18:06:33 +0200

    linked category cards to filters

--- apps/frontend/src/recipes-categories/view/RecipeCategoryCard.tsx
+++ apps/frontend/src/recipes-categories/view/RecipeCategoryCard.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import classNames from 'classnames';
 import { routes } from '@fe/utils';
-import { IRecipeCategoryListItemDto } from '@lib/shared';
+import { CategoryType, IRecipeCategoryListItemDto } from '@lib/shared';
+import { useRecipesFilters } from '@fe/recipes';
 
 interface RecipeCategoryCardProps {
   recipeCategory: IRecipeCategoryListItemDto;
@@ -13,9 +14,34 @@ export const RecipeCategoryCard = ({
   recipeCategory,
   className,
 }: RecipeCategoryCardProps) => {
+  const {
+    selectDishTypeFilter,
+    clearDishTypeFilter,
+    selectCuisineTypeFilter,
+    clearCuisineTypeFilter,
+    selectDietTypeFilter,
+    clearDietTypeFilter,
+    selectOtherCategoryFilter,
+    clearOtherCategoryFilter,
+  } = useRecipesFilters();
+
   return (
     <Link
       href={routes.recipes()}
+      onClick={() => {
+        clearDishTypeFilter();
+        clearCuisineTypeFilter();
+        clearDietTypeFilter();
+        clearOtherCategoryFilter();
+
+        if (recipeCategory.type === CategoryType.DishType)
+          selectDishTypeFilter(recipeCategory.id);
+        else if (recipeCategory.type === CategoryType.CuisineType)
+          selectCuisineTypeFilter(recipeCategory.id);
+        else if (recipeCategory.type === CategoryType.DietType)
+          selectDietTypeFilter(recipeCategory.id);
+        else selectOtherCategoryFilter(recipeCategory.id);
+      }}
       className={classNames(
         'flex rounded-3xl overflow-hidden shadow-md aspect-image items-center justify-center relative',
         className
