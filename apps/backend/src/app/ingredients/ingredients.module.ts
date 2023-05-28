41a99ce893eb96dd1a268a1e3389505d2fa6cc63
Oskar Kupski <oskar.kupski@gmail.com>
2023-05-28 10:37:10 +0200

    fixed relations definition

--- apps/backend/src/app/ingredients/ingredients.module.ts
+++ apps/backend/src/app/ingredients/ingredients.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ingredient } from './entities';
 import { IngredientsService } from './services';
 import { IngredientsRepository } from './repositories';
+import { ShoppingList } from '../shopping-list/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ingredient])],
+  imports: [TypeOrmModule.forFeature([Ingredient, ShoppingList])],
   controllers: [IngredientsController],
   providers: [IngredientsService, IngredientsRepository],
 })
--- apps/backend/src/app/shopping-list/entities/shopping-list.entity.ts
+++ apps/backend/src/app/shopping-list/entities/shopping-list.entity.ts
@@ -1,16 +1,31 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 import { User } from '../../iam/entities';
 import { Ingredient } from '../../ingredients/entities';
+import { IngredientId, UserId } from '@lib/shared';
 
 @Entity()
 export class ShoppingList {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('uuid')
+  user_id: UserId;
+
   @ManyToOne(() => User, (user) => user.shoppingLists)
+  @JoinColumn({ name: 'user_id' })
   user: User;
 
+  @Column('uuid')
+  ingredient_id: IngredientId;
+
   @ManyToOne(() => Ingredient, (ingredient) => ingredient.shoppingLists)
+  @JoinColumn({ name: 'ingredient_id' })
   foodItem: Ingredient;
 
   @Column('decimal')
