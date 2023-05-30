5bf77720a339209a4120320a0d12de8b8c203776
JakubOstaszewski <101040302+JakubOstaszewski@users.noreply.github.com>
2023-05-30 22:06:08 +0200

    updated shopping-list modules

--- apps/backend/src/app/shopping-list/shopping-list.module.ts
+++ apps/backend/src/app/shopping-list/shopping-list.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ShoppingListController } from './shopping-list.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShoppingList } from './entities';
+import { ShoppingListService } from './services';
+import { ShoppingListRepository } from './repositories';
+import { Ingredient } from '../ingredients/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([ShoppingList, Ingredient])],
   controllers: [ShoppingListController],
-  providers: [],
+  providers: [ShoppingListService, ShoppingListRepository],
 })
 export class ShoppingListModule {}
