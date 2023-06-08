1f76ac22928b2a4122a3f225053b0c8efcb3308d
JakubOstaszewski <101040302+JakubOstaszewski@users.noreply.github.com>
2023-06-08 19:36:20 +0200

    added config.py for crawler connection

---
+++ crawler/config.py
@@ -0,0 +1,8 @@
+# Konfiguracja bazy danych
+DB_CONFIG = {
+    host="iacstack-inzynierkadbinstanced4782ce1-hedjbq3rrafm.criw09kq67ql.eu-north-1.rds.amazonaws.com",
+    port=5432,
+    user="postgres",
+    password="xovjaHba1iNoG^C92mI5_7wV5-Hmjv",
+    database="inzynierka"
+}
