b74d476d2928d4c3f8cfe560c1af0ac337036b0d
JakubOstaszewski <101040302+JakubOstaszewski@users.noreply.github.com>
2023-06-08 19:40:40 +0200

    updated recipeInstructionCrawler.py - config, logging, errors

--- crawler/recipeInstructionCrawler.py
+++ crawler/recipeInstructionCrawler.py
@@ -1,74 +1,88 @@
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
 import psycopg2
 import json
 import re
+import logging
+from config import DB_CONFIG
 
-# Connect to the PostgreSQL database
-conn = psycopg2.connect(
-    host="iacstack-inzynierkadbinstanced4782ce1-hedjbq3rrafm.criw09kq67ql.eu-north-1.rds.amazonaws.com",
-    port=5432,
-    user="postgres",
-    password="xovjaHba1iNoG^C92mI5_7wV5-Hmjv",
-    database="inzynierka"
-)
+# Ustawienie poziomu logowania
+logging.basicConfig(level=logging.INFO)
 
-# Create a cursor object
-cursor = conn.cursor()
+def recipe_instruction_crawler():
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(**DB_CONFIG)
 
-# Create the recipedescription table if it does not exist
+    # Create a cursor object
+    cursor = conn.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS crawler_recipeInstruction")
-cursor.execute("CREATE TABLE IF NOT EXISTS crawler_recipeInstruction (id SERIAL PRIMARY KEY, link VARCHAR(255), jsonDescription jsonb)")
+    # Create the recipedescription table if it does not exist
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS crawler_recipeInstruction (id SERIAL PRIMARY KEY, link VARCHAR(255), jsonDescription jsonb)"
+    )
 
-# Select all recipe links from the recipes table
-cursor.execute("SELECT link FROM crawler_recipes")
-links = cursor.fetchall()
+    # Select all recipe links from the recipes table
+    cursor.execute("SELECT link FROM crawler_recipes")
+    links = cursor.fetchall()
 
-# Loop over each recipe link and scrape data
-for link in links:
-    url = 'https://kuchnialidla.pl/' + link[0]
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # Loop over each recipe link and scrape data
+    for link in links:
+        url = "https://kuchnialidla.pl/" + link[0]
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Sprawdzenie, czy wystąpił błąd HTTP
+            soup = BeautifulSoup(response.text, "html.parser")
+        except requests.exceptions.RequestException as e:
+            logging.error("Request failed for link '%s': %s", link[0], e)
+            continue
 
-    print("Processing link:", url)
+        logging.info("Processing link: %s", url)
 
-    # Find the recipe description and save it to the database
-    description_div = soup.find('div', {'class': 'article', 'id': 'opis'})
-    if description_div:
-        result = {'Sposób przygotowania': ''}
-        steps = []
+        # Find the recipe description and save it to the database
+        description_div = soup.find("div", {"class": "article", "id": "opis"})
+        if description_div:
+            result = {"Sposób przygotowania": ""}
+            steps = []
 
-        step_num = 1
+            step_num = 1
 
-        for tag in description_div.children:
-            if tag.name == 'h2' and tag.text == 'Sposób przygotowania:':
-                result['Sposób przygotowania'] = tag.text
-            elif tag.name == 'h2' and ('KROK' in tag.text or 'Krok' in tag.text):
-                try:
-                    step_desc = ''
-                    next_tag = tag.find_next_sibling()
-                    while next_tag and next_tag.name != 'h2':
-                        if next_tag.name == 'p':
-                            # Remove any text between angle brackets
-                            step_desc += re.sub(r'<[^>]+>', '', str(next_tag))
-                        next_tag = next_tag.find_next_sibling()
-                    steps.append({'step': step_desc.strip()})
-                    step_num += 1
-                except AttributeError:
-                    continue
+            for tag in description_div.children:
+                if (
+                    tag.name == "h2"
+                    and ("Sposób przygotowania:" in tag.text or "Sposób przygotowania:" in tag.text)
+                ):
+                    result["Sposób przygotowania"] = tag.text
+                elif (
+                    tag.name == "h2"
+                    and ("KROK" in tag.text or "Krok" in tag.text)
+                ):
+                    try:
+                        step_desc = ""
+                        next_tag = tag.find_next_sibling()
+                        while next_tag and next_tag.name != "h2":
+                            if next_tag.name == "p":
+                                # Remove any text between angle brackets
+                                step_desc += re.sub(r"<[^>]+>", "", str(next_tag))
+                            next_tag = next_tag.find_next_sibling()
+                        steps.append({"step": step_desc.strip()})
+                        step_num += 1
+                    except AttributeError:
+                        continue
 
-        result.update({'Kroki': steps})
+            result.update({"Kroki": steps})
 
-        json_output = json.dumps(steps, ensure_ascii=False, indent=4)
-        insert_query = "INSERT INTO crawler_recipeInstruction (link, jsonDescription) VALUES (%s, %s)"
-        insert_values = (link[0], json_output)
-        cursor.execute(insert_query, insert_values)
+            json_output = json.dumps(result, ensure_ascii=False, indent=4)
+            insert_query = "INSERT INTO crawler_recipeInstruction (link, jsonDescription) VALUES (%s, %s)"
+            insert_values = (link[0], json_output)
+            cursor.execute(insert_query, insert_values)
 
-        # Commit the changes to the database
-        conn.commit()
+            # Commit the changes to the database
+            conn.commit()
 
-# Close the cursor and database connection
-cursor.close()
-conn.close()
+    # Close the cursor and database connection
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    recipe_instruction_crawler()
